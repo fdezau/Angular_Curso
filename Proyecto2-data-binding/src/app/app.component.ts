fe0fb7e723139abecfc6c746839e6be466fe14b9
Francisco Deza <f.dezau0191@gmail.com>
2024-07-17 17:26:40 -0500

    Falta actualizar para ingresar con @for en la table agregar y eliminar

--- Proyecto2-data-binding/src/app/app.component.ts
+++ Proyecto2-data-binding/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import Articulo from './models/articulo.model';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  artículos = [];
+[x: string]: any;
+  articulos: Array<Articulo> = [];
   codigo = 0;
   descripcion = '';
   precio = 0;
 
   agregar(){
-    if (this.codigo || this.descripcion || this.precio){
+    if (!this.codigo || !this.descripcion.length || !this.precio){
       alert('Todos los campos son obligatorios');
         return;
     }
+
+    let articulo: Articulo = new Articulo(this.codigo, this.descripcion, this.precio);
+    this.articulos.push(articulo);
+
+    alert('El artículo se agregó de forma correcta')
   }
 }
