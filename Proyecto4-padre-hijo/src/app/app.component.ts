e40e8450a4af6a95c7c9e56e038a06d33dcf5ede
Francisco Deza <f.dezau0191@gmail.com>
2024-07-18 20:02:45 -0500

    Definida la interfaz para lanzar dados

--- Proyecto4-padre-hijo/src/app/app.component.ts
+++ Proyecto4-padre-hijo/src/app/app.component.ts
@@ -10,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Proyecto4-padre-hijo';
+  valor1: number;
+  valor2: number;
+  valor3: number;
+
+  constructor(){
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+    
+  }
+  generarAleatorio(): number {
+    return Math.trunc(Math.random()*6) + 1;
+  }
 }
