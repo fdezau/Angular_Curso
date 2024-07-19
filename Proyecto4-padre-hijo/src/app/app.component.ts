902cccf891a073ea144a75ccb7bab11b76e9e975
Francisco Deza <f.dezau0191@gmail.com>
2024-07-18 20:25:08 -0500

    Lógica para tirar o lanzar los dados

--- Proyecto4-padre-hijo/src/app/app.component.ts
+++ Proyecto4-padre-hijo/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   valor1: number;
   valor2: number;
   valor3: number;
+  resultado: string;
 
   constructor(){
     this.valor1 = this.generarAleatorio();
@@ -23,4 +24,17 @@ export class AppComponent {
   generarAleatorio(): number {
     return Math.trunc(Math.random()*6) + 1;
   }
+
+  tirar(){
+    this.valor1 = this.generarAleatorio();
+    this.valor1 = this.generarAleatorio();
+    this.valor1 = this.generarAleatorio();
+
+    if(this.valor1 == this.valor2 && this.valor1== this.valor3 && this.valor2 == this.valor3){
+      this.resultado = 'Ganó';
+    } else {
+      this.resultado = 'Perdió'
+    }
+  }
+
 }
